772a196814c442394ba77e6223ffbab352f893d5
GazJack <greta.jackaite@gmail.com>
2025-02-02 18:44:13 +0200

    trecias

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ const app = express();
 //prisijungia prei duombazes
 
 app.use(express.json());//requestam ir responsam
+
+//apsirasyti ROUTES - kelias
+// GET      /products - route -> grazina visus produktus
+// GET      /products - route|kelias -> grazina 1 produkta
+// POST      /products/create -route|kelias -> sukuria viena aprodukta
+// PUT/PATCH      /products/update/:id - route -> redaguoja produkta
+// DELETE      /products/delete/:id -> istrina produkta
+
+app.get('/products', async (req, res)); //req - request, res - response
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Sereris runnina ${PORT}`)
+});
