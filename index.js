3c47d825692ddfbb4761d673a036dde6df200b4e
GazJack <greta.jackaite@gmail.com>
2025-02-02 18:23:57 +0200

    naujinu

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('test');
+
+//express - ekspreso serveris
+//pg - integracia su PSql duombaze
+//dotenv - prisijungimo duomenu pasidejimui duombazėje
+
+const express = require('express'); // require pasiima is modules express
+const app = express();
+
+//prisijungia prei duombazes
+
+app.use(express.json());//requestam ir responsam
